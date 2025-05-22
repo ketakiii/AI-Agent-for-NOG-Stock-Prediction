78d5044e33b6d01058bea885928866c40de564ec
Ketakiii <59907464+ketakiii@users.noreply.github.com>
2025-05-22 17:59:04 -0400

    Add files via upload

--- src/models/embed.py
+++ src/models/embed.py
@@ -1,8 +1,9 @@
 import os 
 import numpy as np
+import pandas as pd
 from openai import OpenAI
 from dotenv import load_dotenv
-
+from sentence_transformers import SentenceTransformer
 
 api_key = os.getenv("OPENAI_API_KEY")
 
@@ -10,6 +11,18 @@ if not api_key:
     raise ValueError("OPENAI_API_KEY not found in environment variables.")
 
 client = OpenAI(api_key=api_key)
+
+def get_news_feature_to_embed():
+    new_df = pd.read_csv('data/NOG_news_2years.csv')
+    content_list = (new_df['title'].fillna('') + '-' + new_df['description'].fillna('')).to_list()
+    return content_list
+
+def embed_news():
+    model = SentenceTransformer()
+    content_list = get_news_feature_to_embed()
+    embeddings = model.encode(content_list, show_progress_bar=True)
+    df_embeddings = pd.DataFrame(embeddings)
+    return df_embeddings
 
 def embed_text(texts):
     """
